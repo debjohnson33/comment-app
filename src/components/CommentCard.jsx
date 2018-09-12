917db26020bab539e179dcab1b58faef67800d0e
debjohnson33 <debjohnson_11@yahoo.com>
2018-09-12 12:56:11 -0500

    Changed CommentCard to include div with props used in it

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
-const CommentCard = _ => <div />
+import PropTypes from 'prop-types'
+
+const CommentCard = ({ comment, author }) => (
+    <div>
+        <p>{comment}</p>
+        <p>- {author}</p>
+    </div>
+) 
+
+CommentCard.propTypes = {
+    comment: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired
+}
 
 export default CommentCard;
