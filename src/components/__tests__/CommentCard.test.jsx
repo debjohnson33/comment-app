14b4736f2903e0a89f6361b040daf66240a2d185
debjohnson33 <debjohnson_11@yahoo.com>
2018-09-12 12:33:09 -0500

    Added test for props to CommentCard test

---
+++ src/components/__tests__/CommentCard.test.jsx
@@ -0,0 +1,9 @@
+describe('Comment Card', () => {
+    TextTrackList('it renders the comment and the author', () => {
+        // Arrange
+        const props = {
+            comment: 'React Testing Library is great',
+            author: 'Luke Ghenco'
+        }
+    })
+})
